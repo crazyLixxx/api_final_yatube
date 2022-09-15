69824d47d2c24fc88c780009430caf2a59febcb2
Konstantin Nikolaev <crazylixx@yandex.ru>
2022-09-15 15:34:47 +0300

    realised api main logic, added foloow model and aoi methods

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,16 +1,38 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 
 
-from posts.models import Comment, Post
+from posts.models import Comment, Follow, Group, Post
+
+User = get_user_model()
 
 
 class PostSerializer(serializers.ModelSerializer):
     author = SlugRelatedField(slug_field='username', read_only=True)
 
     class Meta:
-        fields = '__all__'
         model = Post
+        fields = (
+            'id',
+            'author',
+            'text',
+            'pub_date',
+            'image',
+            'group'
+        )
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = (
+            'id',
+            'title',
+            'slug',
+            'description'
+        )
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -19,5 +41,39 @@ class CommentSerializer(serializers.ModelSerializer):
     )
 
     class Meta:
-        fields = '__all__'
         model = Comment
+        fields = (
+            'id',
+            'author',
+            'post',
+            'text',
+            'created'
+        )
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+        default=serializers.CurrentUserDefault()
+    )
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all(),
+    )
+
+    class Meta:
+        model = Follow
+        fields = ('user', 'following')
+        validators = [
+            serializers.UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=['user', 'following'],
+                message='Этот автор уже есть в ваших подписках.'
+            )
+        ]
+
+    def validate_following(self, value):
+        if self.context.get('request').user == value:
+            raise serializers.ValidationError("Нельзя подписаться на себя")
+        return value
--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,23 @@
-from django.urls import path
+from django.urls import include, path
+
+from rest_framework.authtoken import views
+from rest_framework.routers import DefaultRouter
+
+from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
+
+app_name = 'api'
+
+router = DefaultRouter()
+router.register('v1/posts', PostViewSet)
+router.register('v1/groups', GroupViewSet)
+router.register(
+    r'v1/posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments')
+router.register('v1/follow', FollowViewSet, basename='follow')
+
 
 urlpatterns = [
+    path('', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token),
 ]
